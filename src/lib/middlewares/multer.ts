691e885a92bd0422c99ba725a24c276f6d13d3bc
Francesco Pio Acciarino <kekkoventisei@gmail.com>
2022-10-19 11:58:12 +0200

    add mime and @types/mime to access mimetype of uploaded images, add  generatePhotoFilename function to multer to make uploads readable with correct extensions, add a test for it in multer.test.ts

--- src/lib/middlewares/multer.ts
+++ src/lib/middlewares/multer.ts
@@ -1,7 +1,22 @@
 import multer from "multer";
 
+import mime from "mime";
+
+import { randomUUID } from "node:crypto";
+
+export const generatePhotoFilename = (mimeType: string) => {
+    const fileExtension = mime.getExtension(mimeType);
+    const fileName = `${randomUUID()}-${Date.now()}`;
+    const finalName = `${fileName}.${fileExtension}`;
+
+    return finalName;
+};
+
 const storage = multer.diskStorage({
     destination: "uploads/",
+    filename: (request, file, callback) => {
+        return callback(null, generatePhotoFilename(file.mimetype));
+    },
 });
 
 export const multerOptions = {};
